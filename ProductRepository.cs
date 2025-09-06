f3f605de695734006e84190adbed7ce568f69242
chanan-duy <chanan.duy@proton.me>
2025-09-06 12:45:33 +0500

    Init ProductRepository

---
+++ ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace MP_KT10_2025;
+
+public class ProductRepository : IRepository<Product>
+{
+    private readonly List<Product> _list = [];
+
+    public void Add(Product item)
+    {
+        _list.Add(item);
+    }
+
+    public void Delete(Product item)
+    {
+        _list.Remove(item);
+    }
+
+    public Product? FindById(int id)
+    {
+        return _list.Find(x => x.Id == id);
+    }
+
+    public IEnumerable<Product> GetAll()
+    {
+        return _list;
+    }
+}
