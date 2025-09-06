3634daf9d7874ffce3e5f6e7541509fb2b4f1318
chanan-duy <chanan.duy@proton.me>
2025-09-06 14:19:10 +0500

    Init ComplexNumber

---
+++ ComplexNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace MP_KT10_2025;
+
+public struct ComplexNumber : IComparable<ComplexNumber>
+{
+    public double Real { get; }
+    public double Imaginary { get; }
+
+    public double Magnitude => Math.Sqrt(Real * Real + Imaginary * Imaginary);
+
+    public ComplexNumber(double real, double imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+
+    public int CompareTo(ComplexNumber other)
+    {
+        return Magnitude.CompareTo(other.Magnitude);
+    }
+}
