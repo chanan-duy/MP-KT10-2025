87352cbbf709e8cc21a2d7afb5f3947ab1c20368
chanan-duy <chanan.duy@proton.me>
2025-09-06 14:13:19 +0500

    Init RationalNumberComparer

---
+++ RationalNumberComparer.cs
@@ -0,0 +1,19 @@
+﻿namespace MP_KT10_2025;
+
+public class RationalNumberComparer : IComparer<RationalNumber>
+{
+    public int Compare(RationalNumber x, RationalNumber y)
+    {
+        return x.CompareTo(y);
+    }
+
+    public string CompareToString(RationalNumber x, RationalNumber y)
+    {
+        return Compare(x, y) switch
+        {
+            0 => "==",
+            < 0 => "<",
+            _ => ">",
+        };
+    }
+}
