4cdc8065acceb77008a2d2353e1196c86e5217f5
chanan-duy <chanan.duy@proton.me>
2025-09-06 13:51:57 +0500

    Init Rectangle

---
+++ Rectangle.cs
@@ -0,0 +1,29 @@
+﻿namespace MP_KT10_2025;
+
+public class Rectangle : IClonable<Rectangle>
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public Rectangle(Rectangle other)
+    {
+        Width = other.Width;
+        Height = other.Height;
+    }
+
+    public Rectangle Clone()
+    {
+        return new Rectangle(this);
+    }
+
+    public override string ToString()
+    {
+        return $"W={Width} H={Height}";
+    }
+}
