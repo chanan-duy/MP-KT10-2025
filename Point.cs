6dcc1c3fce552a2a9fbe51b1ef4d2d4fa06e2354
chanan-duy <chanan.duy@proton.me>
2025-09-06 13:46:05 +0500

    Init Point

---
+++ Point.cs
@@ -0,0 +1,29 @@
+﻿namespace MP_KT10_2025;
+
+public class Point : IClonable<Point>
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point(Point other)
+    {
+        X = other.X;
+        Y = other.Y;
+    }
+
+    public Point Clone()
+    {
+        return new Point(this);
+    }
+
+    public override string ToString()
+    {
+        return $"X={X} Y={Y}";
+    }
+}
