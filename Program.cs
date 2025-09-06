b65749fb734f3d8a47e3b52c1e038951b1bdd986
chanan-duy <chanan.duy@proton.me>
2025-09-06 13:05:10 +0500

    Test CustomerRepository

--- Program.cs
+++ Program.cs
@@ -1,9 +1,40 @@
 ﻿namespace MP_KT10_2025;
 
+public static class MainLogic
+{
+    private static void TestRepositories()
+    {
+        var cusRepo = new CustomerRepository();
+
+        cusRepo.Add(new Customer { Id = 1, Address = "addr1", Name = "name1" });
+        cusRepo.Add(new Customer { Id = 2, Address = "addr2", Name = "name2" });
+        cusRepo.Add(new Customer { Id = 3, Address = "addr3", Name = "name3" });
+
+        var found = cusRepo.FindById(2);
+        if (found != null)
+        {
+            cusRepo.Delete(found);
+        }
+
+        foreach (var customer in cusRepo.GetAll())
+        {
+            Console.WriteLine($"{customer.Id}\t{customer.Name}\t{customer.Address}");
+        }
+
+        var prodRepo = new ProductRepository();
+    }
+
+    public static void RunLogic()
+    {
+        TestRepositories();
+        Console.Write(Environment.NewLine);
+    }
+}
+
 public static class Program
 {
     public static void Main(string[] _)
     {
-        Console.WriteLine("Hello, World!");
+        MainLogic.RunLogic();
     }
 }
