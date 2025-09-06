7622cf967b183d3a89cfd1f00a76cc128b4c0cde
chanan-duy <chanan.duy@proton.me>
2025-09-06 12:45:24 +0500

    Init CustomerRepository

---
+++ CustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace MP_KT10_2025;
+
+public class CustomerRepository : IRepository<Customer>
+{
+    private readonly List<Customer> _list = [];
+
+    public void Add(Customer item)
+    {
+        _list.Add(item);
+    }
+
+    public void Delete(Customer item)
+    {
+        _list.Remove(item);
+    }
+
+    public Customer? FindById(int id)
+    {
+        return _list.Find(x => x.Id == id);
+    }
+
+    public IEnumerable<Customer> GetAll()
+    {
+        return _list;
+    }
+}
