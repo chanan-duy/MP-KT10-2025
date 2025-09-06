144f86f1ef9758e81f3504b84681d7e84541687b
chanan-duy <chanan.duy@proton.me>
2025-09-06 14:25:49 +0500

    Init ComplexNumberComparer

---
+++ ComplexNumberComparer.cs
@@ -0,0 +1,19 @@
+﻿namespace MP_KT10_2025;
+
+public class ComplexNumberComparer : IComparer<ComplexNumber>
+{
+    public int Compare(ComplexNumber x, ComplexNumber y)
+    {
+        return x.CompareTo(y);
+    }
+
+    public string CompareToString(ComplexNumber x, ComplexNumber y)
+    {
+        return Compare(x, y) switch
+        {
+            0 => "==",
+            < 0 => "<",
+            _ => ">",
+        };
+    }
+}
