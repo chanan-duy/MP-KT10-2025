18fc4960305f0b5bdf5e84e8ab412784e96d8961
chanan-duy <chanan.duy@proton.me>
2025-09-06 13:08:24 +0500

    Test ProductRepository

--- Program.cs
+++ Program.cs
@@ -2,26 +2,52 @@
 
 public static class MainLogic
 {
-    private static void TestRepositories()
+    private static void TestCustomerRepository()
     {
-        var cusRepo = new CustomerRepository();
+        var prodRepo = new CustomerRepository();
 
-        cusRepo.Add(new Customer { Id = 1, Address = "addr1", Name = "name1" });
-        cusRepo.Add(new Customer { Id = 2, Address = "addr2", Name = "name2" });
-        cusRepo.Add(new Customer { Id = 3, Address = "addr3", Name = "name3" });
+        prodRepo.Add(new Customer { Id = 1, Address = "addr1", Name = "name1" });
+        prodRepo.Add(new Customer { Id = 2, Address = "addr2", Name = "name2" });
+        prodRepo.Add(new Customer { Id = 3, Address = "addr3", Name = "name3" });
 
-        var found = cusRepo.FindById(2);
+        var found = prodRepo.FindById(2);
         if (found != null)
         {
-            cusRepo.Delete(found);
+            prodRepo.Delete(found);
         }
 
-        foreach (var customer in cusRepo.GetAll())
+        foreach (var customer in prodRepo.GetAll())
         {
             Console.WriteLine($"{customer.Id}\t{customer.Name}\t{customer.Address}");
         }
+    }
 
+
+    private static void TestProductRepository()
+    {
         var prodRepo = new ProductRepository();
+
+        prodRepo.Add(new Product { Id = 1, Price = 200.2, Name = "name1" });
+        prodRepo.Add(new Product { Id = 2, Price = 202.2, Name = "name2" });
+        prodRepo.Add(new Product { Id = 3, Price = 705.2, Name = "name3" });
+
+        var found = prodRepo.FindById(1);
+        if (found != null)
+        {
+            prodRepo.Delete(found);
+        }
+
+        foreach (var customer in prodRepo.GetAll())
+        {
+            Console.WriteLine($"{customer.Id}\t{customer.Name}\t{customer.Price}");
+        }
+    }
+
+    private static void TestRepositories()
+    {
+        TestCustomerRepository();
+        Console.Write(Environment.NewLine);
+        TestProductRepository();
     }
 
     public static void RunLogic()
