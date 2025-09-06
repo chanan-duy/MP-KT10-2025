c063652e9fed1062fdc34fd12bd9e6cc839f0a32
chanan-duy <chanan.duy@proton.me>
2025-09-06 12:27:01 +0500

    Init IRepository

---
+++ IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace MP_KT10_2025;
+
+public interface IRepository<T> where T : IEntity
+{
+    public void Add(T item);
+    public void Delete(T item);
+    public T FindById(int id);
+    public IEnumerable<T> GetAll();
+}
