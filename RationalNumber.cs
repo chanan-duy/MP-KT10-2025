d5ed3aa101cc75fd7061e1af20ec70cd9aa179b6
chanan-duy <chanan.duy@proton.me>
2025-09-06 14:13:10 +0500

    Init RationalNumber

---
+++ RationalNumber.cs
@@ -0,0 +1,21 @@
+﻿namespace MP_KT10_2025;
+
+public struct RationalNumber : IComparable<RationalNumber>
+{
+    public long Numerator { get; set; }
+    public long Denominator { get; set; }
+
+    public RationalNumber(long numerator, long denominator)
+    {
+        Numerator = numerator;
+        Denominator = denominator;
+    }
+
+    public int CompareTo(RationalNumber other)
+    {
+        var thisNumerator = Numerator * other.Denominator;
+        var otherNumerator = other.Numerator * Denominator;
+
+        return thisNumerator.CompareTo(otherNumerator);
+    }
+}
